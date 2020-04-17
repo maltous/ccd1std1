3b122fc185f9a5ec9a077deff82a76d9170d1d38
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-04-17 17:36:12 +0300

    ccd1std1

---
+++ story_content/user.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6e4pDB2eEjt":
+        Script1();
+        break;
+      case "6pRZteGlAwj":
+        Script2();
+        break;
+      case "6lZO6wYBwmj":
+        Script3();
+        break;
+      case "62OlsZEYP4J":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  window.print();
+}
+
